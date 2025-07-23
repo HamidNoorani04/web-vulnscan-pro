5ce79ddceb201add1470d09fba78cfeb24e1a30e
Abdul Hamid Noorani <hamidnoorani04@gmail.com>
2025-07-23 20:40:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import json
+import requests
 
 def save_report(data, filename):
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
+
+def login_session(login_url, username, password):
+    session = requests.Session()
+    data = {"username": username, "password": password}
+    session.post(login_url, data=data) 
+    return session
